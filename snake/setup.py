51fb49a925505799b8edb7338c7f1c891e47b4f7
Eliran Turgeman <50831652+DevEliran@users.noreply.github.com>
2019-10-17 21:58:32 +0300

    Delete setup.py

--- snake/setup.py
+++
@@ -1,8 +0,0 @@
-import cx_Freeze
-
-executables=[cx_Freeze.Executable("snake.py")]
-cx_Freeze.setup(name="Snake",
-                 options={"build_exe":{"packages":["pygame"],
-                                         "include_files":["snake.py",r"8.jpg","high_score.txt",r"1.mp3",r"2.mp3",r"1.jpg",r"2.jpg",r"3.jpg",r"4.jpg",r"5.jpg",r"6.jpg",r"7.jpg"
-                                                            ]}},
-                 executables=executables)
